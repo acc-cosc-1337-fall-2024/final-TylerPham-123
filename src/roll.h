ff7812d850871fb09becb611c86743e7294b883b
Tyler <minhphu.pham@g.austincc.edu>
2024-12-12 23:49:17 +0000

    Question 2

--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,25 @@
 //
+#ifndef ROLL_H
+#define ROLL_H
+
+#include "die.h"
+
+class Roll {
+public:
+    Roll(Die& d1, Die& d2) : die1(d1), die2(d2) {}
+
+    void roll_dice() {
+        rolled_value = die1.roll() + die2.roll();
+    }
+
+    int roll_value() const {
+        return rolled_value;
+    }
+
+private:
+    Die& die1;
+    Die& die2;
+    int rolled_value;
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -18,3 +18,18 @@ TEST_CASE("DieTest, RollInRange")
         REQUIRE(roll <= 6);
     }
 }
+
+#include "roll.h"
+
+TEST_CASE("RollTest, RollInRange")
+{
+    Die die1, die2;
+    Roll roll(die1, die2);
+
+    for (int i = 0; i < 10; ++i) {
+        roll.roll_dice();
+        int roll_value = roll.roll_value();
+        REQUIRE(roll_value >= 2);
+        REQUIRE(roll_value <= 12);
+    }
+}
