e87e6daea472ebef32e6c746821a2bbfa3ce2b67
Tyler <minhphu.pham@g.austincc.edu>
2024-12-12 22:46:20 +0000

    final 1

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -9,29 +9,13 @@ TEST_CASE("Verify Test Configuration", "verification")
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Test die; Test 1: Roll 10 times")
+TEST_CASE("DieTest, RollInRange")
 {
-	Die die;
-	for (int i = 0; i < 10; ++i)
+    Die die;
+    for (int i = 0; i < 10; ++i)
 	{
-		int roll = die.roll();
-		assert(roll >= 1 && roll <= 6);
-	}
-
-	REQUIRE(die.roll() >= 1);
-	REQUIRE(die.roll() <= 6);
-}
-
-TEST_CASE("Test die; Test 2: Roll 1000 times")
-{
-	Die die;
-	int counts[6] = {0};
-	for (int i = 0; i < 1000; ++i)
-	{
-		int roll = die.roll();
-		counts[roll - 1]++;
-	}
-
-	REQUIRE(die.roll() >= 1);
-	REQUIRE(die.roll() <= 6);
+        int roll = die.roll();
+		REQUIRE(roll >= 1);
+        REQUIRE(roll <= 6);
+    }
 }
